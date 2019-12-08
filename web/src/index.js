42f5778e93751383d583453bba951642ae356060
Victor Tiunov <vtiunov@dynamika.ru>
2019-12-08 14:49:09 +0300

    minor changes in style for companies list

--- web/src/index.js
+++ web/src/index.js
@@ -9,8 +9,9 @@ import rootSaga from "./sagas/root.saga";
 
 import './css/index.css';
 import './css/login.css';
-import './css/rules_tables.css';
+import './css/companies.css';
 import './css/roles.css';
+import './css/rules_tables.css';
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './App';
