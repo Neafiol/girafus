a15a2c130d0af0fdfbccca61e89e256faf2badda
Petr <neafiol@ya.ru>
2019-12-08 14:37:59 +0300

    ser

---
+++ web/src/components/role_editor.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {List} from 'semantic-ui-react';
+import RulesTable from './rules_table';
+
+class UsersList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedUser: '',
+        }
+    }
+
+    handleSelectUser = userId => {
+        this.setState({
+            selectedUser: this.state.selectedUser === userId ? undefined : userId
+        }, () => {
+            let selectedUser = this.state.selectedUser;
+            if (selectedUser && !this.props.users[selectedUser].rules) {
+                this.props.getUserRules(selectedUser);
+            }
+        });
+    };
+
+    showRules = userId =>
+        this.state.selectedUser === userId;
+
+    render() {
+        return (
+            <List selection animated verticalAlign='middle'>
+                {Object.values(this.props.users).map(user =>
+                        <List.Item key={`user_${user.id}`}>
+                            <List.Header onClick={() => this.handleSelectUser(user.id)}>
+                                {`${user.name} - ${user.position}`}
+                            </List.Header>
+                            {this.showRules(user.id) && <RulesTable rows={user.rules}/>}
+                        </List.Item>
+                )}
+            </List>
+        )
+    }
+}
+
+export default UsersList;
